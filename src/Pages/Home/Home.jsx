54a7faddcf8a825136ef73f3922d4d897cd8dfc6
Dennis Wachira <91716907+Bush-Man@users.noreply.github.com>
2023-02-02 03:53:34 +0300

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
           <div className="Home">
         <div className="profileSide">
           <div className="profileCard">
-           <button onClick={handleNavigation} style={{background:"white",border:"none",outline:"none"}}></button>
+           <button onClick={handleNavigation} style={{background:"white",border:"none",outline:"none"}}>.</button>
             <div className="profImages">
                  
                 <img className="coverImage" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo8PBlksM5ycqP8YRhDcsl853_G5tcwZKt5AS8yGJoIw&s" alt="" />
@@ -47,8 +47,15 @@ const Home = () => {
               <h3>About Me</h3>
               <hr style={{width:80,marginBottom:4}} />
               <p>
-                <span style={{fontSize:25}}>L</span>orem ipsum dolor sit amet consectetur adipisicing elit. Veniam ipsa omnis iste reprehenderit nihil culpa animi voluptatem blanditiis corrupti, labore suscipit earum voluptates optio ex repellat voluptatum, magnam magni soluta.
-              </p>  
+                <span style={{fontSize:25}}>H</span>i I am Dennis a Fullstack web developer having been in this field for more than 2 years as a freelancer i am able to develop for you your next big website using the MERN Stack which allows devlopment of both the front-end part and back-end part of your website. MERN Stack is a mnemonic word where M stands for Mongo.db which is a database,while E represents Express.js which is a serverside node.js framework and R for React.js which builds the front-end part finally N Node.js.The main advantages of developing your website using MERN Stack are:
+                 <br>
+                     1. Development takes a short time.<br>
+                     2. Your website is more secure.<br>
+                     3. Easy to update.<br>
+                     4. Low maintainance cost.<br>
+                     5. Fast.<br>
+               Welcome..
+               </p>  
             
             </div>
               
